3e1c3081198734100f91cbdad43742f313043391
EduGollner0609 <dudugollner@gmail.com>
2025-05-02 18:15:06 -0300

    Metódos auxiliares para criar e fechar conexão com o banco.

---
+++ TodoList/Database/Database.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using TodoList.Database.Exceptions;
+
+namespace TodoList.Database
+{
+    internal class Database
+    {
+        private const string connectionString =
+        "Server=EDUARDO;Database=todolist;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            try
+            {
+                SqlConnection conn = new(connectionString);
+                return conn;
+            }
+            catch (SqlException e)
+            {
+                throw new DatabaseException("Erro: " + e.Message);
+            }
+        }
+
+        public static void CloseConnection(SqlConnection conn)
+        {
+            try
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            catch (SqlException e)
+            {
+                throw new DatabaseException("Erro: " + e.Message);
+            }
+        }
+    }
+}
--- TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f33ca535c0385a9f8cabfe9df64ae9ee774f19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60ae753a48d7359928591793ea87675ec3fe107")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
