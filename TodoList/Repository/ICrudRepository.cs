3ddf35d09481d66f7b4bd0f9495765d600d59bf2
EduGollner0609 <dudugollner@gmail.com>
2025-05-02 18:35:42 -0300

    Criando interface crudrepository com metódos CRUD.

---
+++ TodoList/Repository/ICrudRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Repository
+{
+    internal interface ICrudRepository <T>
+    {
+        // Create
+        void Insert(T entity);
+
+        // Read
+        T FindById(int id);
+        List<T> FindAll();
+
+        // Update
+        void Update(T entity, int id);
+
+        //Delete
+        void DleeteById(int id);
+    }
+}
