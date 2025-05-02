6362ccaba6c58bd9cacfe12d7c6c80e85d9d34bb
EduGollner0609 <dudugollner@gmail.com>
2025-05-02 17:58:56 -0300

    Adicionando pacote/biblioteca ADO.NET.

--- TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b13008be3d2cd28751ddbd020580911b3de9bd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
